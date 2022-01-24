64c14b068a8ed43d5b326c7d725ac7ffd4e5abed
Shahray156 <shahray1561@gmail.com>
2022-01-24 13:08:48 +0300

    1 task

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,28 @@ package com.company;
 
 public class Main {
 
+    // task1
+    public static void leapYear(int year) {
+        boolean leapYear = isleapYear(year);
+        printIsleapYear(year);
+    }
+
+    private static boolean isleapYear(int year) {
+        return (((year % 400 == 0) && (year % 100 != 0)) || (year % 4 == 0));
+    }
+
+    private static void printIsleapYear(int year) {
+        if (isleapYear(year)) {
+            System.out.println(year + " високосный год");
+        } else {
+            System.out.println(year + " не високосный год");
+        }
+    }
+
     public static void main(String[] args) {
+        printIsleapYear(2013);
 
     }
 }
+
+
