64e605e9da041ae2484be50b9fab3df68e3467fd
Shahray156 <shahray1561@gmail.com>
2022-01-24 15:02:10 +0300

    3 task

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.time.LocalDate;
+
 public class Main {
 
     // task1
@@ -22,7 +24,42 @@ public class Main {
 
     public static void main(String[] args) {
         printIsleapYear(2013);
+        printRecommendation(0, 2013);
+        System.out.println();
+        deliveryDays (14);
+        System.out.println("Для доставки требуется " + deliveryDays( 14) + " дней");
 
+    }
+    //task 2
+
+    public static void printRecommendation(int clientOS, int deviceYear) {
+        boolean releaseDevice = isReleaseDevice(deviceYear);
+        System.out.println("Установите ");
+        if (releaseDevice) {
+            System.out.println("lite");
+        }
+        System.out.println("версию для ");
+        if (clientOS == 0) {
+            System.out.println("iOS");
+        } else {
+            System.out.println("Andriond");
+        }
+    }
+
+    private static boolean isReleaseDevice(int deviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        return deviceYear <= currentYear;
+    }
+
+    // task3
+    public static int deliveryDays(int distance) {
+        int days = 1;
+         if (distance >= 20 && distance <= 60) {
+            days++;
+        } else if (distance >= 60 && distance < 100) {
+            days +=2;
+        }
+        return days;
     }
 }
 
